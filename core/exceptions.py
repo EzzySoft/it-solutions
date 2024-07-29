aaf9c201d821eab56c2d4c1347ff859f99978590
Danila Tretiakov <daniyatretyakov@gmail.com>
2024-07-29 15:00:53 +0300

    add exceptions.py

---
+++ core/exceptions.py
@@ -0,0 +1,23 @@
+class InvalidSessionError(ValueError):
+    def __init__(self):
+        super().__init__("Invalid session ID")
+
+
+class InvalidCredentialsError(ValueError):
+    def __init__(self):
+        super().__init__("Invalid credentials")
+
+
+class UsernameInUseError(ValueError):
+    def __init__(self):
+        super().__init__("The username is already taken")
+
+
+class EmailInUseError(ValueError):
+    def __init__(self):
+        super().__init__("The email is already taken")
+
+
+class UserNotFoundError(ValueError):
+    def __init__(self):
+        super().__init__("User not found")
