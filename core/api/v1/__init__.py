9ac6af0a0e9fd19779b4fc7e3321da78728a0cb3
Danila Tretiakov <daniyatretyakov@gmail.com>
2024-07-29 15:56:18 +0300

    update routers

--- core/api/v1/__init__.py
+++ core/api/v1/__init__.py
@@ -2,7 +2,9 @@ from fastapi import APIRouter
 from config import settings
 
 from .auth import router as auth_router
+from .advertisement import router as ad_router
 
 router = APIRouter(prefix=settings.api.v1.prefix)
 
 router.include_router(router=auth_router, prefix=settings.api.v1.auth)
+router.include_router(router=ad_router, prefix=settings.api.v1.ad_router)
